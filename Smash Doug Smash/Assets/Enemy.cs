7605656d506c356e98152594fb13ca415f3acc56
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2021-10-22 10:27:58 -0500

    IT WORKSSSSS

--- Smash Doug Smash/Assets/Enemy.cs
+++ Smash Doug Smash/Assets/Enemy.cs
@@ -1,58 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    public float speed;
-    internal int Length;
-    private GameObject player;
-   
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-
-        player = GameObject.Find("Player");
-
-        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-
-        transform.Translate(lookDirection * Time.deltaTime * speed);
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-
-        if (other == GameObject.Find("Player"))
-        {
-            Destroy(gameObject);
-
-            Destroy(GameObject.Find("Player"));
-
-            Debug.Log("Game Over");
-
-        }
-        else if (other != GameObject.Find("Player"))
-        {
-
-            Debug.Log("You will die soon!!!");
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+ public class Enemy : MonoBehaviour
+ {
+ 
+     public Transform Player;
+     int MoveSpeed = 4;
+     int MaxDist = 10;
+     int MinDist = 5;
+ 
+ 
+ 
+ 
+     void Start()
+     {
+ 
 
 
 
-        }
+     }
+ 
+     void Update()
+     {
+         transform.LookAt(Player);
+ 
+         if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+         {
+ 
+             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+ 
+ 
+ 
+             if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+             {
+                 
 
 
-    }
 
-}
+
+
+             }
+ 
+         }
+     }
+ }

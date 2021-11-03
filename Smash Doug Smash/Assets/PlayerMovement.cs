11aed91c7eb24d917a7104a0ec1e354e201bc17a
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2021-11-03 10:41:18 -0500

    nothing new

--- Smash Doug Smash/Assets/PlayerMovement.cs
+++ Smash Doug Smash/Assets/PlayerMovement.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 0.05f;
-    public float speed = 0.01f;
+    public Rigidbody playerRb;
+    private float turnSpeed = 10f;
+    private float speed = 1f;
     private float horizontalInput;
     private float verticalInput;
     
 
     void Start()
     {
-        
+
+        playerRb.GetComponent<Rigidbody>();
 
     }
 
 
 
-    void Update()
+    void FixedUpdate()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
-        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        playerRb.transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        playerRb.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
     
 
--- Smash Doug Smash/Assets/SpawnManager.cs
+++ Smash Doug Smash/Assets/SpawnManager.cs
@@ -42,7 +42,7 @@ public class SpawnManager : MonoBehaviour
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 1, spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
     }
 
